57e5d97811f5dd87844ec19b7dd111d0c28779c2
Karlo Bartels <karlo@bartelsonline.nl>
2021-05-06 16:08:13 +0200

    Project setup

---
+++ GISBlox.Services.SDK/GISBlox.Services.SDK/GISBloxClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GISBlox.Services.SDK
+{
+   public class GISBloxClient : IDisposable
+   {
+      public void Dispose()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
