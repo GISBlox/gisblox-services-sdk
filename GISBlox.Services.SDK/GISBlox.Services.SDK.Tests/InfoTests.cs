aa9fee52d19bea8191c4538031c7ab42181e0314
Karlo Bartels <karlo@bartelsonline.nl>
2023-11-15 13:39:18 +0100

    Added Info tests

---
+++ GISBlox.Services.SDK/GISBlox.Services.SDK.Tests/InfoTests.cs
@@ -0,0 +1,24 @@
+﻿namespace GISBlox.Services.SDK.Tests
+{
+   [TestClass]
+   public class InfoTests
+   {
+      const string BASE_URL = "https://services.gisblox.com";
+
+      [TestMethod]
+      public async Task GetInfo()
+      {
+         // Get the service key from the test.runsettings file
+         string serviceKey = Environment.GetEnvironmentVariable("ServiceKey");
+
+         // Create the service client object
+         using (var client = GISBloxClient.CreateClient(BASE_URL, serviceKey))
+         {
+            List<Subscription> subscriptions = await client.Info.GetSubscriptions();
+            subscriptions.ForEach(sub => Console.WriteLine($"\r\nName: {sub.Name} \r\nDescription: {sub.Description} \r\nRegistration date: {sub.RegisterDate} Expiration date: {sub.ExpirationDate} Expired: {sub.Expired}"));
+            
+            Assert.IsTrue(subscriptions.Any());
+         }
+      }
+   }
+}
